ccba9c3426633a031e6597174e22ba37e7eb506b
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-12-22 17:56:32 +0100

    updated

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bookModel = require("../models/book.model");
-const bookValidatorMiddleWare = require("../validators/book.validator");
+const {
+  AddBookValidationMiddleWare,
+  UpdateBookValidationMiddleWare
+} = require("../validators/book.validator");
 const bookRouter = express.Router();
 
 bookRouter.get("/", async (req, res) => {
   try {
-    const books = await bookModel.find();
+    const books = await (await bookModel.find()).reverse();
     res.send(books);
   } catch (error) {
     console.log(error);
@@ -25,7 +28,7 @@ bookRouter.get("/:id", async (req, res) => {
   }
 });
 
-bookRouter.post("/", bookValidatorMiddleWare, async (req, res) => {
+bookRouter.post("/", AddBookValidationMiddleWare, async (req, res) => {
   const book = req.body;
 
   try {
@@ -38,15 +41,19 @@ bookRouter.post("/", bookValidatorMiddleWare, async (req, res) => {
   }
 });
 
-bookRouter.put("/:id", bookValidatorMiddleWare, async (req, res) => {
+bookRouter.put("/:id", UpdateBookValidationMiddleWare, async (req, res) => {
   const id = req.params.id;
   const book_to_be_updated = req.body;
-  book.updatedAt = new Date();
+  book_to_be_updated.updatedAt = Date.now();
 
   try {
-    const updatedBook = await bookModel.findByIdAndUpdate(id, book, {
-      new: true
-    });
+    const updatedBook = await bookModel.findByIdAndUpdate(
+      id,
+      book_to_be_updated,
+      {
+        new: true
+      }
+    );
 
     res.status(200).send(updatedBook);
   } catch (error) {
@@ -60,9 +67,9 @@ bookRouter.delete("/:id", async (req, res) => {
   const id = req.params.id;
 
   try {
-    const deletedBook = await bookModel.findByIdAndRemove(id);
+    const deletedBook = await bookModel.findByIdAndDelete(id);
 
-    res.status(200).json(deletedBook, "book deleted successfully");
+    res.status(200).json({ message: "book deleted successfully" });
   } catch (error) {
     console.log(error);
 
